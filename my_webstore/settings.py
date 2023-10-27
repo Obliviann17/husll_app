38231d9c5edc0016f9cf30d59b1cf43ab78bf5f5
Rostislav <denysenkorostislav@gmail.com>
2023-10-28 02:06:17 +0300

    deployment on vercel

--- my_webstore/settings.py
+++ my_webstore/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@#6_-2!6kk^j4%gd^#!f7zq1+k1=&2@3=j6=7p(jt#v!0qzbte'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
